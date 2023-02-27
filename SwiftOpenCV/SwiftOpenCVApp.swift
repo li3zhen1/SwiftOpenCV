f493f9713a532503121cb344ab87a88291659dac
li3zhen1 <master_lee@live.com>
2023-02-27 13:13:37 +0800

    Initial Commit

---
+++ SwiftOpenCV/SwiftOpenCVApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftOpenCVApp.swift
+//  SwiftOpenCV
+//
+//  Created by 李臻 on 2/27/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftOpenCVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
